9d0ef08f4a3986bea2a4d2dc69b3df0900c32438
Pawel <pawel.bornikowski@gmail.com>
2024-01-05 00:28:21 +0100

    feat: add save data btn to show json

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 
 
 import EditorComponent from "@/components/EditorComponent";
+import EditorJS from "@editorjs/editorjs";
+
 
 export default function Home() {
-  console.log('home')
+ 
+ 
+
   return (
     <>
       <h2 className="mb-20 text-4xl">Case Creator</h2>
       <div className="w-3/4 h-3/4 bg-white pt-4">
         <EditorComponent />
       </div>
+     <div id="save-btn-portal"></div>
     </>
   );
 }
--- components/EditorComponent.tsx
+++ components/EditorComponent.tsx
@@ -1,12 +1,24 @@
-'use client'
-import React, { useEffect, useRef } from "react";
-
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
 import EditorJS from "@editorjs/editorjs";
 import { EDITOR_JS_TOOLS } from "@/tools/tools";
 
 const EditorComponent = () => {
-    console.log('editor')
-    const ejInstance = useRef<EditorJS | null>();
+  const ejInstance = useRef<EditorJS | null>();
+
+  const handleSave = () => {
+    ejInstance?.current?.save().then((outputData) => {
+      console.log("Article data: ", outputData);
+    });
+  };
+
+  const [saveButtonPortalElement, setSaveButtonPortalElement] =
+    useState<Element | null>(null);
+
+  useEffect(() => {
+    setSaveButtonPortalElement(document.getElementById("save-btn-portal"));
+  }, []);
   const initEditor = () => {
     const editor = new EditorJS({
       holder: "editorjs",
@@ -14,7 +26,6 @@ const EditorComponent = () => {
         ejInstance.current = editor;
       },
       tools: EDITOR_JS_TOOLS,
-     
     });
   };
 
@@ -22,7 +33,7 @@ const EditorComponent = () => {
     if (ejInstance.current === null) {
       initEditor();
     }
-    
+
     return () => {
       ejInstance?.current?.destroy();
       ejInstance.current = null;
@@ -32,6 +43,16 @@ const EditorComponent = () => {
   return (
     <>
       <div id="editorjs"></div>
+      {saveButtonPortalElement &&
+        createPortal(
+          <button
+            className="px-4 py-2 text-white rounded-lg bg-blue-600"
+            onClick={handleSave}
+          >
+            Save Data
+          </button>,
+          saveButtonPortalElement
+        )}
     </>
   );
 };
